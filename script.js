91cab94d4a6efbd0feeb03a489d65a5204bcd34d
Jennifer H <74622778+jh80@users.noreply.github.com>
2024-04-16 10:08:51 -0700

    Add a way to change background with 2 functions
    This way is incomplete at this point.

---
+++ script.js
@@ -0,0 +1,8 @@
+blueBtn.addEventListener("click", () => {
+    body.setAttribute("class", "blue");
+    heading.textContent = `This is a but litte site with a blue background`;
+});
+greenBtn.addEventListener("click", () => {
+    body.setAttribute("class", "green");
+    heading.textContent = `This is a but litte site with a green background`;
+});
