834b207a94cc61490b3d070dabc80bc8bfbb3c55
Jennifer H <74622778+jh80@users.noreply.github.com>
2024-04-16 18:19:06 -0700

    Delete unused code

--- script.js
+++ script.js
@@ -6,21 +6,7 @@ const body = document.querySelector("body");
 
 blueBtn.addEventListener("click", () => color("blue") )
 
-// blueBtn.addEventListener("click", () => {
-//     body.setAttribute("class", "blue");
-//     heading.textContent = `This is a but litte site with a blue background`;
-// });
-// greenBtn.addEventListener("click", () => {
-//     body.setAttribute("class", "green");
-//     heading.textContent = `This is a but litte site with a green background`;
-// });
-
 function color (color) {
     body.setAttribute("class", color);
     heading.textContent = `This is a but litte site with a ${color} background`;    
 }
-
-
-
-    
-// (color === "blue" ? "blue" : "green")
